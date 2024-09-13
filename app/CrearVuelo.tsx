b5f1993805981f8743a321361f59b353b8f29acc
Santito268 <santiagocarrillopineros@gmail.com>
2024-09-12 20:39:28 -0500

    Agrega componentes

---
+++ app/CrearVuelo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CrearVuelo = () => {
+  return (
+    <div>
+      <h2>Crear Vuelo</h2>
+      {/* Aquí va el formulario o contenido para crear un vuelo */}
+    </div>
+  );
+};
+
+export default CrearVuelo;
---
+++ app/EliminarVuelo.tsx
@@ -0,0 +1,13 @@
+// app/EliminarVuelo.tsx
+import React from 'react';
+
+const EliminarVuelo = () => {
+  return (
+    <div>
+      <h2>Eliminar Vuelo</h2>
+      {/* Aquí va el formulario o contenido para eliminar un vuelo */}
+    </div>
+  );
+};
+
+export default EliminarVuelo;
